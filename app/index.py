10a75bd69e6ec6c3109a9f5f35a3f31fda301fd4
coolerputt01 <olumideadekolu9@gmail>
2026-05-01 13:29:08 -0700

    Signup page created

--- app/index.py
+++ app/index.py
@@ -1,19 +1,34 @@
-from ui.views import index_view
+from ui.views import index_view,signup_view
 import flet as ft
+import asyncio
 
-def main(page: ft.Page):
+async def main(page: ft.Page):
     page.fonts = {
         "Pixel": "fonts/pixel.ttf"
     }
-    def route_change():
+    async def load_app():
+        await page.push_route("/signup")
+        page.update()
+    
+    async def route_change():
         if page.route == '/':
-            page.views.append(index_view)
+            page.views.clear()
+            page.views.append(index_view())
+            await asyncio.sleep(2)
+
+            await load_app()
+        
+        elif page.route == "/signup":
+            page.views.clear()
+            page.views.append(signup_view())
+            page.update()
     
         page.update()
     
     page.on_route_change = route_change
     page.push_route('/')
+    
 
-    route_change()
+    await route_change()
 
 ft.run(main,assets_dir="assets")
--- app/ui/views.py
+++ app/ui/views.py
@@ -1,26 +1,72 @@
 import flet as ft
 
-index_view = ft.View(
-    route="/",
-    vertical_alignment=ft.MainAxisAlignment.CENTER,
-    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-    controls=[
-        ft.Column(
-            controls=[
-                ft.Image(
-                    src="img/illustrations/blink.gif",
-                    width=200,
-                    filter_quality=ft.FilterQuality.HIGH,
-                    border_radius=10
-                ),
-                ft.Text(
-                    "Textes",
-                    font_family="Pixel",
-                    size=48,
-                    weight=ft.FontWeight.BOLD
-                )
-            ],
-            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-        )
-    ]
-)
+
+def index_view():
+    return ft.View(
+        route="/",
+        vertical_alignment=ft.MainAxisAlignment.CENTER,
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        controls=[
+            ft.Column(
+                controls=[
+                    ft.Image(
+                        src="img/illustrations/blink.gif",
+                        width=200,
+                        filter_quality=ft.FilterQuality.HIGH,
+                        border_radius=10
+                    ),
+                    ft.Text(
+                        "Textes",
+                        font_family="Pixel",
+                        size=48,
+                        weight=ft.FontWeight.BOLD
+                    )
+                ],
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            )
+        ]
+    )
+
+def signup_view():
+    return ft.View(
+        route="/signup",
+        vertical_alignment=ft.MainAxisAlignment.CENTER,
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        controls=[
+            ft.Column(
+                controls=[
+                    ft.Text("Enter Username",font_family="Pixel",size=10,text_align=ft.TextAlign.LEFT,color=ft.Colors.GREY_700),
+                    ft.TextField(
+                        value="",
+                        text_align=ft.TextAlign.LEFT,
+                        width=350,
+                        autocorrect=False,
+                        hint_text="Type something...",
+                        text_style=ft.TextStyle(
+                            font_family="Pixel",
+                            size=14
+                        ),
+                    ),
+                    ft.Button(
+                        content="Okay!",
+                        width=360,
+                        margin=ft.margin.only(top=10),
+                        style=ft.ButtonStyle(
+                            shape=ft.RoundedRectangleBorder(radius=20),
+                            bgcolor={
+                                ft.ControlState.DEFAULT: ft.Colors.GREEN_200,
+                                ft.ControlState.HOVERED: ft.Colors.GREEN_400,
+                                ft.ControlState.PRESSED: ft.Colors.GREEN_600,
+                            },
+                            color=ft.Colors.WHITE,
+                            padding=ft.Padding(20, 10, 20, 10),
+                            text_style=ft.TextStyle(
+                                font_family="Pixel",
+                                size=12
+                            ),
+                        ),
+                    )
+                ]
+            )
+        ]
+    )
